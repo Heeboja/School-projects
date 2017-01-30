34315f21029d6973d29be54e2c0584ccf4703d80
Heeboja <samun91@hotmail.com>
2017-01-30 16:12:36 +0200

    Create Ex3-1.c

---
+++ Embedded C-programming/Ex3-1.c
@@ -0,0 +1,34 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 23.1.2017 16:27:24
+ * Author : Samu Haapoja
+ */ 
+
+#define F_CPU 1000000UL // CPU speed @ 1 MHz
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int i = 0; // intialize a counter
+
+ISR(TIMER0_OVF_vect) {
+	i++; // increase counter every ~256 ms
+		if (i == 4) { 
+			PORTB = PORTB^0x01;
+			i = 0;
+		}
+}
+// Requires a CLK of 1MHz
+int main(void) {
+	DDRB = 0xFF;  // PORTB as input
+	PORTB = 0xFF; // PORTB default values off
+	TCCR0 = TCCR0 | 0x05; // 0000 0101
+	TIMSK = TIMSK | 0x01 ; // 0000 0001
+	sei(); // Enables global interrupts
+	while (1) {
+		// endless loop	
+	}
+	
+	return 0;
+}
