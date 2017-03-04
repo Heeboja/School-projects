d5f5db7cccc089ee3de2f777079a8f510f5e7f52
Heeboja <samun91@hotmail.com>
2017-03-04 13:19:51 +0200

    Create Harkka2b.java

---
+++ Tietorakenteet/Harkka2b/Harkka2b.java
@@ -0,0 +1,16 @@
+package bst;
+
+/*Testaus ja "käyttöliittymä"*/
+class Harkka2b {
+	public static void main(String args[]) {
+		BinarySearchTree BST = new BinarySearchTree();
+		BST.Insert(3, 505); 
+		BST.Insert(2, 328);
+		BST.Insert(5, 509); 
+		BST.Insert(1, 328);
+		BST.Insert(4, 509); 
+		BST.Print();
+		
+	}
+
+}
