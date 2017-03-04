96fd03124a647c94e9747adfd70f401abe4d1093
Heeboja <samun91@hotmail.com>
2017-03-04 21:05:55 +0200

    Update Harkka2b.java

--- Tietorakenteet/Harkka2b/Harkka2b.java
+++ Tietorakenteet/Harkka2b/Harkka2b.java
@@ -1,16 +1,51 @@
 package bst;
 
+import java.util.Scanner; //käytetään scannerluokkaa lukemaan konsolista
+
+
 /*Testaus ja "käyttöliittymä"*/
 class Harkka2b {
+	
+	private static Scanner scan;
+	
 	public static void main(String args[]) {
-		BinarySearchTree BST = new BinarySearchTree();
-		BST.Insert(3, 505); 
-		BST.Insert(2, 328);
-		BST.Insert(5, 509); 
-		BST.Insert(1, 328);
-		BST.Insert(4, 509); 
-		BST.Print();
 		
+		scan = new Scanner(System.in);
+		System.out.println("Binary search tree!\n");          
+        char ch; //valinta jatkosta
+		BinarySearchTree BST = new BinarySearchTree(); // puun alustus
+		
+		
+		do {
+			System.out.println("\nAvailable Operations\n");
+	        System.out.println("1. Insert");
+	        System.out.println("2. Display");
+	        System.out.println("Enter operation: \n");
+	        
+	        int choice = scan.nextInt();   
+	        
+	        switch (choice) {
+	        	
+	        	case 1 : // Datan syöttö. Ensin avain, sitten data.
+	        			System.out.println("\nInsert key [int] : \n");
+	        			int k = scan.nextInt();
+	        			System.out.println("\nInsert data [int] : \n");
+	        			int d = scan.nextInt();
+	        			BST.Insert(k, d);
+	        			break;
+	        	case 2 : // Datan tulostus avaimien järjestyksessä.
+	        			System.out.println("\nComplete tree from left to right: \n");
+	        			BST.Print();
+	        			break;
+	        	default :
+	        			System.out.println("Wrong Entry \n "); // Tyhmien käyttäjien varalta.
+	        			break;
+	        }
+	        
+	        System.out.println("\nDo you want to continue (Type y or n) \n"); // jatkamisen ohjeistus
+            ch = scan.next().charAt(0);  
+	            	
+		} while (ch == 'Y'|| ch == 'y'); // jatketaan ainoastaan jos jompikumpi noista syötetään.
 	}
 
 }
